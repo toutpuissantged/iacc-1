c125e962badddedbc55f7c37542b4ea57ea06fb6
robertotsaf <nguefackpaul86@gmail.com>
2022-02-22 05:21:40 +0100

    page d'acceuil mis a jour, possibilité de creer un fichier homme statique seul pour la presentation globale de l'acceuil

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { useState, useEffect } from "react";
 import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
 import { Features } from "./components/features";
-import { About } from "./components/about";
+import { Home } from "./components/home";
 import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
 import { Team } from "./components/Team";
-import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
@@ -28,12 +25,9 @@ const App = () => {
       <Navigation />
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery}/>
-      <Testimonials data={landingPageData.Testimonials} />
+      <Home data={landingPageData.Home}/>
       <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
     </div>
   );
 };
