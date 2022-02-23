97ae0f5a9923e8af56710cde821fa16b1cc83cbd
toutpuissantged <amoussougedeon13@gmail.com>
2022-02-23 09:48:11 +0000

    mis en place du routages et des pages initiaux

---
+++ src/router/map.js
@@ -0,0 +1,10 @@
+// RouteMap est un objet javascript qui nous permet de définir les différentes routes de notre application et de les associer à des clefs pour eviter la modification  repetitif lors de la modification de la routes vers une page 
+const RouteMap = {
+
+    home : '/',
+    account : '/account',
+    blog : '/blog',
+
+}
+
+export default RouteMap;
