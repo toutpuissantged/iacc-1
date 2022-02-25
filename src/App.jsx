217ff9e6d0a90013a5d852ac39941a3d5a1d5c86
toutpuissantged <amoussougedeon13@gmail.com>
2022-02-25 14:35:09 +0000

    correction du conflit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
-import { useState, useEffect } from "react";
-import { Navigation } from "./components/navigation";
-import { Features } from "./components/features";
-import { Home } from "./components/home";
-import { ServicesHome } from "./components/servicehome";
-import { BlogHome } from "./components/bloghome";
-import { RegistrerHome } from "./components/registrerhome";
-import { Footer } from "./components/footer";
-import { Slider } from "./components/slider";
-import JsonData from "./data/data.json";
-import SmoothScroll from "smooth-scroll";
-import "./App.css";
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+import Router from './router'
 
-export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
-});
-
-const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
-
+export default function App() {
   return (
-    <div>
-      <Navigation />
-      <Slider data={landingPageData.Slider} />
-      <Features data={landingPageData.Features} />
-      <ServicesHome data={landingPageData.ServicesHome} />
-      <Home data={landingPageData.Home}/>
-      <BlogHome data={landingPageData.BlogHome} />
-      <RegistrerHome data={landingPageData.RegistrerHome} />
-      <Footer data={landingPageData.Footer} />
-    </div>
-  );
-};
-
-export default App;
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
+}
